ebce4f6d61621771070a61e5f880933aa435e233
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-06 14:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+
+      function scuberGreetingForFeet (feet) {
+      
+      let result
+      
+      if (feet <=400) {
+      result = "This one is on me!";}
+        
+      else if (feet >2000) {
+      result = "I will gladly take your thirty bucks.";  
+      }
+        
+      else (feet >2500) {
+        result = "No can do.";
+      }
+      
+      return result
+        
+      }
+      scuberGreetingForFeet (199);
+      scuberGreetingForFeet (199);
+      scuberGreetingForFeet (199);
