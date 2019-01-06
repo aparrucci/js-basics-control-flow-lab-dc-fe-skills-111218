fa84bab46ea1aaa3f0f24832d882b3b12d4495bf
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-06 14:39:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function scuberGreetingForFeet (feet) {
     return `I will gladly take your thirty bucks.`;
   }
   
-  else (feet > 2500) {
+  else if (feet > 2500) {
     return `No can do.`;
   }
 }
