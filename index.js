85fb6ee1223c241e290430c779c0fa668e67dd88
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-06 14:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
       let scuberGreetingForFeet;
       
       if (feet <=400) {
-      result = "This one is on me!";}
+      scuberGreetingForFeet = "This one is on me!";}
         
       else if (feet >2000) {
       result = "I will gladly take your thirty bucks.";  
