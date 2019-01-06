04542a1ab5132af946ef3fff3ba78778542a9c7b
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-06 15:37:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ return (city === 'NYC') ? `Ok, sounds good.` : `No go.`;
 }
 
 const tip = 'generous';
-let tipAmount;
+let tipFeedback;
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
